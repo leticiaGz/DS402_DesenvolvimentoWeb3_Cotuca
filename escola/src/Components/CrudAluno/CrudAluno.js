2c5eef9570c86247c10fa04be4bbd7ba1efd5e18
leticia zaneti <leticiazaneti03@gmail.com>
2022-09-26 15:59:57 -0300

    H

--- escola/src/Components/CrudAluno/CrudAluno.js
+++ escola/src/Components/CrudAluno/CrudAluno.js
@@ -3,9 +3,11 @@ import './CrudAluno.css';
 import Main from '../Template/Main/Main';
 import axios from 'axios';
 
+
 const title = "Cadastro de Alunos";
 
 const urlAPI = "https://localhost:7129/api/aluno";
+
 const initialState = {
     aluno: { id: 0, ra: '', nome: '', codCurso: 0 },
     lista: []
@@ -15,20 +17,22 @@ export default class CrudAluno extends Component {
 
     state = { ...initialState }
 
+    // recebendo get de todo mundo e colocaando na lista
     componentDidMount() {
         axios(urlAPI).then(resp => {
             this.setState({ lista: resp.data })
         })
     }
 
+    // limpando dado do aluno
     limpar() {
         this.setState({ aluno: initialState.aluno });
     }
 
     salvar() {
-        const aluno = this.state.aluno;
-        aluno.codCurso = Number(aluno.codCurso);
-        const metodo = aluno.id ? 'put' : 'post';
+        const aluno = this.state.aluno; // pego meu aluno
+        aluno.codCurso = Number(aluno.codCurso); //transformo em number
+        const metodo = aluno.id ? 'put' : 'post'; // se tiver id é 
         const url = aluno.id ? `${urlAPI}/${aluno.id}` : urlAPI;
 
 
@@ -40,8 +44,8 @@ export default class CrudAluno extends Component {
     }
 
     getListaAtualizada(aluno, add = true) {
-        const lista = this.state.lista.filter(a => a.id !== aluno.id);
-        if (add) lista.unshift(aluno);
+        const lista = this.state.lista.filter(a => a.id !== aluno.id); // cria um novo arryb so com quem pas no teste
+        if (add) lista.unshift(aluno); // adiciona no começo do vetor
         return lista;
     }
 
