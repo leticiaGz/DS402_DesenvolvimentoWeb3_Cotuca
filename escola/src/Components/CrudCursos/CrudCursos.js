048dcd0f6b4af5781110f014cbd24a430bdac5cc
leticia zaneti <leticiazaneti03@gmail.com>
2022-09-28 16:13:51 -0300

    front quase finalizado

--- escola/src/Components/CrudCursos/CrudCursos.js
+++ escola/src/Components/CrudCursos/CrudCursos.js
@@ -1,129 +1,170 @@
 
 
-import "./CrudCursos.css";
+import "./CrudCurso.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Main from '../Template/Main/Main';
 
  export default function CrudCursos() {
 
- 
-  
-  {/*Criando componente*/}
-  const Cursos = ({ cursos }) => {
-    return (
-
-      //fazendo o map das todos
-      <div className="todos">
-        {todos.map((curso) => {
-          return (
-            <div className="todo">
-
-              <p>{curso.codCurso}</p>
-              <button onClick={() =>{} /*handleWithEditButtonClick(todo)*/}>
-                remover
-              </button>
-              <button onClick={() =>{} /*deleteTodo(todo)*/}>
-                editar
-              </button>
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-
-  const [todos, setTodos] = useState([]); //onde lista meus cursos
-  const [inputValue, setInputValue] = useState(""); // pegando o valor do meu input
-  const [inputVisbility, setInputVisility] = useState(false); // estado da visibilidade dos inputs
-  const [selectedTodo, setSelectedTodo] = useState(); // utilizada no update, para ativar o input
+  const [cursos, setCursos] = useState([]);
+  const [inputCodCurso, setInputCodCurso] = useState("");
+  const [inputNome, setInputNome] = useState(""); 
+  const [inputPeriodo, setInputPeriodo] = useState("");
+  const [inputVisbility, setInputVisility] = useState(false);
+  const [selectedCurso, setSelectedCurso] = useState();
   const api = "https://localhost:7129/api/curso";
 
   useEffect(() => {
-    getTodos(); // chamando a função do select que lista os cursos
+    getCursos(); // chamando a função do select que lista os cursos
   }, []);
 
-//faz meu input aparecer quando o botao é clicado
-  /*async function handleWithNewButton() {
-    console.log("fasfas");
+  async function getCursos() {
+    const response = await axios.get(api);
+      setCursos(response.data);
+  }
+
+  async function deleteCurso(curso) {
+   await axios.delete(`${api}/${curso.id}`);
+      getCursos();
+  }
+
+  async function createCurso() {
+    await axios.post(api, {
+        codCurso: inputCodCurso,
+        nomeCurso: inputNome,
+        periodo: inputPeriodo
+    });
+    getCursos(); // atualiza a pagina
+    setInputVisility(!inputVisbility);//sumir o input
+    setInputNome("");
+    setInputCodCurso("");
+    setInputPeriodo("");
+  }
+
+  async function limpaInput() {
+    setInputNome("");
+    setInputCodCurso("");
+    setInputPeriodo("");
     setInputVisility(!inputVisbility);
-  }*/
+
+  }
+
+  //faço o put
+ async function editCurso() {
+
+   await axios.put(`${api}/${selectedCurso.id}`, {
+      codCurso: inputCodCurso,
+      nomeCurso: inputNome,
+      periodo: inputPeriodo
+    });
+    setSelectedCurso();
+    setInputVisility(false);
+    getCursos();
+    setInputNome("");
+    setInputCodCurso("");
+    setInputPeriodo(""); //limpando input
+  }
+
+
+  //faz a tabela aparecer quando o botao é clicado
+  async function handleWithNewButton() {
+    setInputVisility(!inputVisbility);
+  }
 
   //quando clica no botao habilita o input
- /* async function handleWithEditButtonClick(todo) {
-    setSelectedTodo(todo);
+  async function handleWithEditButtonClick(curso) {
+    setSelectedCurso(curso);
     setInputVisility(true);
-  }*/
-
-  //faço get cursos *****
-  async function getTodos() {
-    const response = await axios.get(api);
-    setTodos(response.data);
-    //console.log(response.data);
   }
-//faço o put
- /*async function editTodo() {
-    const response = await axios.put("http://localhost:3333/todos", {
-      id: selectedTodo.id,
-      name: inputValue,
-    });
-    setSelectedTodo();
-    setInputVisility(false);
-    getTodos();
-    setInputValue(""); // linpando o input
-  }*/
-// delete
- /* async function deleteTodo(todo) {
-    const response = await axios.delete(
-      `http://localhost:3333/todos/${todo.id}`
-    );
-    getTodos();
-  }*/
 
+  {/*Criando componente que renderiza os cursos*/}
+ 
+  const Cursos = ({ cursos }) => {
+    return (
 
+    <div className="listagem">
+      <table className="listaAlunos" id="tblListaAlunos">
+        <thead>
+          <tr className="cabecTabela">
+            <th className="tabTituloRa">CodCurso</th>
+            <th className="tabTituloNome">NomeCurso</th>
+            <th className="tabTituloCurso">periodo</th>
+          </tr>
+        </thead>
+          <tbody>
+            {cursos.map((curso) =>
+              <tr key={curso.id}>
+                <td>{curso.codCurso}</td>
+                <td>{curso.nomeCurso}</td>
+                <td>{curso.periodo}</td>
+                <td>
+                  <button onClick={() =>{handleWithEditButtonClick(curso)}}>
+                    Editar
+                  </button>
+                </td>
+                <td>
+                <button onClick={() =>{deleteCurso(curso)}}>
+                  remover
+                </button>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
 
-  /*async function createTodo() {
-    const response = await axios.post("http://localhost:3333/todos", {
-      name: inputValue,
-    });
-    getTodos(); // atualiza a pagina
-    setInputVisility(!inputVisbility);//sumir o input
-    setInputValue("");
-  }*/
+    )
+  };
 
-
-
+  {/*Criando componente que renderiza o Forms*/}
+  const Form = () => {
   return (
-    <div className="App">
-      <header className="container">
-        <div className="header">
-          <h1>Dont be lazzy</h1>
-        </div>
+    <div >
+      <div>
+ 
+        <div className="inclui-container" style={{ display: inputVisbility ? "inline-block" : "none" /*se for true mostro se nao escondo*/ }}>
 
-        {/* chamando a const todos e passando suas props*/}
-        <Cursos cursos={curso}></Cursos>
+        <label>Codigo do Curso:</label>
+        <input value={inputCodCurso} className="form-input" placeholder="hhh" type="number"
+        onChange={(event) => {setInputCodCurso(event.target.value); /* setando valor do input*/}}></input>
 
-      {/*Deixanndo o input visivel ou não, alterando o display atraves da variavel*/}
+        <label>Nome do Curso:</label>
+        <input value={inputNome} className="form-input" type="text"
+        onChange={(event) => {setInputNome(event.target.value); /* setando valor do input*/}}></input>
 
-        <input
-          value={inputValue}
-          style={{ display: inputVisbility ? "block" : "none" /*se for true mostro se nao escondo*/ }}
-          onChange={(event) => {
-            setInputValue(event.target.value); // setando valor do input
-          }}
-          className="inputName"
-        ></input>
+        <label>Periodo:</label>
+        <input value={inputPeriodo} className="form-input" type="text"
+        onChange={(event) => {setInputPeriodo(event.target.value); /* setando valor do input*/}}></input>
 
        {/*Botao novo curso*/}  
-
-       {/*Se estiver selected é update se nao é put*/} 
-        <button
-          onClick={inputVisbility? selectedTodo ? editTodo: createTodo: handleWithNewButton}
-          className="newTaskButton"
-        >
-          {inputVisbility ? "Confirm" : "+ New task" /* se a visibilidade for true retorno confirm se nao new task*/ }
+       <button  className="btnSalvar"
+          onClick={inputVisbility? selectedCurso ? editCurso: createCurso: handleWithNewButton}>
+          Salvar
         </button>
-      </header>
+
+        <button  className="btnCancelar"
+          onClick={()=> {limpaInput()}}>
+          cancelar
+        </button>
+
+        </div>
+          {/*Se estiver selected é update se nao é put*/} 
+          <button  className="btnSalvar" style={{ display: inputVisbility ? "none" : "block"}}
+            onClick={inputVisbility? selectedCurso ? editCurso: createCurso: handleWithNewButton}>
+            novo curso
+        </button>
+
+      </div>
     </div>
-  );
+  );}
+
+  return(
+    <Main >
+      <Form></Form>
+      <Cursos cursos={cursos}></Cursos>
+      
+    </Main>
+  )
 }
 
